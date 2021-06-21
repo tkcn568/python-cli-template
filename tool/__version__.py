510a328f384b39a958ce65e8732f75e03a013dce
donald <dw@angk.org>
2021-06-21 18:33:58 -0500

    build: add dockerignore

--- tool/__version__.py
+++ tool/__version__.py
@@ -1,5 +1,9 @@
 import os
+import toml
 
-__pkgname__ = 'clitool'
+
+__buildname__ = 'tool'
+__details = toml.load('../pyproject.toml')[__buildname__]['poetry']
+__pkgname__ = __details['name']
 __location__ = os.path.split(os.path.dirname(os.path.realpath('./__version__.py')))[-1]
-__version__ = '2020.04.30.dev1'
+__version__ = __details['version']
